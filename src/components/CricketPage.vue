3830033da71df343570bee01d12bbb1d434ffb59
Titouan CAZIN <Titouan CAZIN@LAPTOP-3HF8CSPD>
2023-06-06 17:49:50 +0200

    Cricket ok

--- src/components/CricketPage.vue
+++ src/components/CricketPage.vue
@@ -1,29 +1,54 @@
 <template>
     <v-container>
+        <v-row>
+            <v-col cols="3">
+            </v-col>
+            <v-col
+                v-for="(player, i) in $store.state.players"
+                :key="i"
+                cols="3"
+            >
+                {{ player.name }}
+            </v-col>
+        </v-row>
         <v-row
-            v-for="n in 5"
-            :key="n"
+            v-for="(data, index) in points_available"
+            :key="index"
         >
             <v-col
                 cols="3"
             >
-                {{ 20-n }}
+                {{ data["name"] }}
             </v-col>
             <v-col
-                v-for="i in 3"
+                v-for="(player, i) in $store.state.players"
                 :key="i"
                 cols="3"
                 style="background-color: blue;"
-                
-                @click="handleClick(n, i)"
+                @click="handleClick(index, i, data['value'])"
             >
             <v-responsive :aspect-ratio="1 / 1">
-                <svg v-if="typeof closingBoxes[n] !== 'undefined'" height="100%" width="100%">
-                    <circle v-if="closingBoxes[n][i] >= 3" cx="50%" cy="50%" r="40%" stroke="black" stroke-width="8%" fill="transparent" />
-                    <line v-if="closingBoxes[n][i] >= 1" x1="20%" y1="80%" x2="80%" y2="20%" stroke="black" stroke-width="8%" />
-                    <line v-if="closingBoxes[n][i] >= 2" x1="20%" y1="20%" x2="80%" y2="80%" stroke="black" stroke-width="8%" />
+                <svg v-if="typeof $store.state.cricket_closings[index] !== 'undefined'" height="100%" width="100%">
+                    <circle v-if="$store.state.cricket_closings[index][i] >= 3" cx="50%" cy="50%" r="40%" stroke="black" stroke-width="8%" fill="transparent" />
+                    <line v-if="$store.state.cricket_closings[index][i] >= 1" x1="20%" y1="80%" x2="80%" y2="20%" stroke="black" stroke-width="8%" />
+                    <line v-if="$store.state.cricket_closings[index][i] >= 2" x1="20%" y1="20%" x2="80%" y2="80%" stroke="black" stroke-width="8%" />
                 </svg>
             </v-responsive>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col
+                cols="3"
+            >
+                Points
+            </v-col>
+            <v-col
+                v-for="(player, i) in $store.state.players"
+                :key="i"
+                cols="3"
+                style="background-color: red;"
+            >
+                {{ $store.state.cricket_points[i] }}
             </v-col>
         </v-row>
     </v-container>
@@ -33,14 +58,28 @@ export default {
     name: 'CricketPage',
     data() {
         return {
-            closingBoxes: new Array(5).fill(new Array(3).fill(0))
+            points_available: [
+                {value: 20, name: '20'},
+                {value: 19, name: '19'},
+                {value: 18, name: '18'},
+                {value: 17, name: '17'},
+                {value: 16, name: '16'},
+                {value: 15, name: '15'},
+                {value: 25, name: 'Bull'}
+            ]
         }
     },
     methods: {
-        handleClick(test, i){
-            this.closingBoxes[test][i] += 1;
-            console.log(test, i)
-            console.log(this.closingBoxes)
+        handleClick(index_points, i, points_to_add){
+            if(this.$store.state.cricket_closings[index_points][i] < 3)
+                this.$store.state.cricket_closings[index_points][i] += 1;
+            else{
+                this.$store.state.players.forEach( function (_, i_player) {
+                    if(this.$store.state.cricket_closings[index_points][i_player] < 3){
+                        this.$store.state.cricket_points[i_player] += points_to_add
+                    }
+                }, this)
+            }
         }
     }
 }
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,8 @@ export default createStore({
   state: {
     players: [{name:'Titouan'},{name:'Timothé'},{name:'Michel'}],
     limit_x01 : "301",
+    cricket_closings : new Array(7).fill(0).map(() => new Array(4).fill(0)),
+    cricket_points : new Array(4).fill(0),
   },
   getters: {
   },
